ad9a2596ec572bc91621f800c731915334d5d6d6
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-04-22 17:44:11 +0400

    style: change EditFeedback page's styles for mobile, tablet and desktop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ bgColor, text, icon, onclick }) => {
   return (
     <button
       onClick={onclick}
-      className={`${bgColor} text-white-100 md:text-[14px] text-[13px] font-bold py-[12px] px-[25px] rounded-[10px] flexCenter transition-all`}
+      className={`${bgColor} text-white-100 md:text-[14px] text-[13px] font-bold py-[12px] px-[25px] rounded-[10px] flexCenter transition-all  lg:hover:bg-[#C75AF6] lg:transition lg:duration-200`}
     >
       {icon}
       {text}
--- src/components/cancelButton.jsx
+++ src/components/cancelButton.jsx
@@ -1,14 +1,13 @@
 const CancelButton = ({ bgColor, text, icon, onclick }) => {
-    return (
-      <button
-        onClick={onclick}
-        className={`${bgColor} text-white-100 text-[14px] font-bold py-[12px] px-[25px] rounded-[10px] flexCenter `}
-      >
-        {icon}
-        {text}
-      </button>
-    );
-  };
-  
-  export default CancelButton;
-  
+  return (
+    <button
+      onClick={onclick}
+      className={`${bgColor} text-white-100 text-[14px] font-bold py-[12px] px-[25px] rounded-[10px] flexCenter w-full  md:w-[93px] lg:hover:bg-[#656EA3] lg:transition lg:duration-200`}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default CancelButton;
